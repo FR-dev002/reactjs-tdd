95daef6cb22774d93fcb4b5f15648d1753f87030
ram <fikriramadhan@gmail.com>
2020-08-10 22:51:51 +0700

    add enzyme and eslint

---
+++ test/unit/app.spec.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { mount } from "enzyme";
+import App from "./../../src/App";
+
+describe("component App", () => {
+   it("render Hello World", () => {
+      const wrapper = mount(<App />);
+      expect(wrapper.find('.App').text()).toContain("Hello World");
+   })
+});
